99265c611118fe3c91b1346d66df58f3114ec7b9
joshlynch2001 <joshlynch2001@gmail.com>
2025-02-13 15:49:47 +0000

    added logo and fixed all styling to look very professional!

--- src/HeaderNav/headernav.jsx
+++ src/HeaderNav/headernav.jsx
@@ -1,12 +1,13 @@
 import "./headernav.css";
 import React from 'react';
+import Logo from "../assets/logo.png"
 
 function HeaderNav() {
   return (
     <div className='HeadNavMain'>
         <div className="BrandDetails">
             <div className='LogoContainer'>
-                <img></img>
+                <img src={Logo} className="logoImg"></img>
             </div>
             <h3 className='Title'>CTR Daily Run</h3>
         </div>
